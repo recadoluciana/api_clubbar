d3d5b3b26bf8982d0bac29e295aedc44dd416004
Crispim <recadoluciana@gmail.com>
2026-03-27 23:06:28 -0300

    eu hein

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI(title="clubbar API")
 
 from app.routers import auth
+from app.routers import lojas
+
+app.include_router(auth.router)
+
+from app.routers import auth
 
 app.include_router(auth.router)
 
