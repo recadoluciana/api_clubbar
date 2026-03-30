3273ef93735452ec751d2459bfe43a9382537385
Crispim <recadoluciana@gmail.com>
2026-03-30 14:59:35 -0300

    correção cadastro organizacao 2

--- app/models/organizacao.py
+++ app/models/organizacao.py
@@ -2,15 +2,32 @@ from sqlalchemy import Column, BigInteger, String, DateTime
 from sqlalchemy.sql import func
 from app.database import Base
 
+
 class Organizacao(Base):
     __tablename__ = "organizacao"
 
     organizacao_id = Column(BigInteger, primary_key=True, autoincrement=True)
+
     nmorganizacao = Column(String(120), nullable=False)
     cnpjorganizacao = Column(String(18), nullable=True)
     emailorganizacao = Column(String(255), nullable=True)
     telorganizacao = Column(String(25), nullable=True)
-    sitorganizacao = Column(String(15), nullable=False, default="ATIVA")
 
-    dtcriacao = Column(DateTime, server_default=func.now(), nullable=False)
-    dtultatu = Column(DateTime, onupdate=func.now())
+    sitorganizacao = Column(
+        String(15),
+        nullable=False,
+        server_default="ATIVA"
+    )
+
+    dtcriacao = Column(
+        DateTime,
+        nullable=False,
+        server_default=func.now()
+    )
+
+    dtultatu = Column(
+        DateTime,
+        nullable=True,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
