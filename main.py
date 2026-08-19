1a3af3b3edaaf414f1bd8461e0a052457f1a071c
Luciana Correa <recadoluciana@gmail.com>
2026-08-19 12:49:49 -0300

    correção da aabertura da tela de eventos/lotes 2

--- main.py
+++ main.py
@@ -48,6 +48,10 @@ logger = logging.getLogger(__name__)
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",    
+    "http://localhost:3002",
+    "http://127.0.0.1:3002",    
     "http://localhost:5500",
     "http://127.0.0.1:5500",
 
