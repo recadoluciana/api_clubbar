b09dec39e52acdf11fe8cf5bace9567902c516f0
Luciana Correa <recadoluciana@gmail.com>
2026-09-19 03:41:20 -0300

    create schema com mais um campo sittitular

--- scripts/database/migrations/20260919_titularfinanceiro_status.sql
+++ scripts/database/migrations/20260919_titularfinanceiro_status.sql
@@ -1,5 +1,5 @@
 ALTER TABLE titularfinanceiro
-  ADD COLUMN IF NOT EXISTS sittitular VARCHAR(15) NOT NULL DEFAULT 'ATIVO';
+  ADD COLUMN sittitular VARCHAR(15) NOT NULL DEFAULT 'ATIVO';
 
-CREATE INDEX IF NOT EXISTS ix_titularfinanceiro_sittitular
+CREATE INDEX ix_titularfinanceiro_sittitular
   ON titularfinanceiro (sittitular);
